f9deee7ec53791dd556ea1acefcae72735a51610
aron-alvarenga <aron.alvarenga@hotmail.com>
2022-07-19 00:27:15 -0400

    Function to delete task + Snackbar was added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
+// ignore_for_file: null_check_always_fails, deprecated_member_use, import_of_legacy_library_into_null_safe
+
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: const Home(),
       title: 'ToDo App',
+      theme: ThemeData(
+        snackBarTheme: const SnackBarThemeData(
+          actionTextColor: Colors.white,
+        ),
+      ),
     ),
   );
 }
@@ -24,6 +32,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
   List _toDoList = [];
+  Map<String, dynamic>? _lastRemoved;
+  int? _lastRemovedPos;
 
   @override
   void initState() {
@@ -37,7 +47,7 @@ class _HomeState extends State<Home> {
 
   void _addToDo() {
     setState(() {
-      Map<String, dynamic> newToDo = Map();
+      Map<String, dynamic> newToDo = {};
       newToDo["title"] = _toDoController.text;
       _toDoController.text = "";
       newToDo["isDone"] = false;
@@ -46,77 +56,138 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<void> _refresh() async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((x, y) {
+        if (x["isDone"] && !y["isDone"]) {
+          return 1;
+        } else if (!x["isDone"] && y["isDone"]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+      _saveData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('To Do App'),
-          backgroundColor: Colors.red,
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _toDoController,
-                      decoration: const InputDecoration(
-                        labelText: 'New task',
-                        labelStyle: TextStyle(
-                          color: Colors.red,
-                        ),
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red),
-                        ),
-                        border: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red),
-                        ),
+      appBar: AppBar(
+        title: const Text('To Do App'),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _toDoController,
+                    decoration: const InputDecoration(
+                      labelText: 'New task',
+                      labelStyle: TextStyle(
+                        color: Colors.red,
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red),
                       ),
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: _addToDo,
-                    child: const Icon(Icons.plus_one),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red,
-                    ),
+                ),
+                ElevatedButton(
+                  onPressed: _addToDo,
+                  child: const Icon(Icons.plus_one),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Expanded(
+          ),
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: _refresh,
               child: ListView.builder(
                 padding: const EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
-                itemBuilder: (context, index) {
-                  return CheckboxListTile(
-                    title: Text(_toDoList[index]["title"]),
-                    value: _toDoList[index]["isDone"],
-                    secondary: CircleAvatar(
-                      child: Icon(
-                        _toDoList[index]["isDone"] ? Icons.check : Icons.error,
-                        color: Colors.white,
-                      ),
-                      backgroundColor: Colors.red,
-                    ),
-                    onChanged: (bool? value) {
-                      setState(() {
-                        _toDoList[index]["isDone"] = value;
-                        _saveData();
-                      });
-                    },
-                  );
-                },
+                itemBuilder: buildItem,
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildItem(BuildContext context, int index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: const Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["isDone"],
+        secondary: CircleAvatar(
+          child: Icon(
+            _toDoList[index]["isDone"] ? Icons.check : Icons.error,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.red,
+        ),
+        onChanged: (bool? value) {
+          setState(() {
+            _toDoList[index]["isDone"] = value;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+          final snack = SnackBar(
+            content: Text('Task "${_lastRemoved!["title"]}" was removed!'),
+            action: SnackBarAction(
+                label: "Undo Action",
+                // textColor: Colors.white,
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos!, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: const Duration(seconds: 2),
+            backgroundColor: Colors.red,
+          );
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
   }
 
   Future<File> _getFile() async {
